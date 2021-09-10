9144def429170b7f94c0d213a76541c0fe1d5946
carolynegreen <carolynemarie94@gmail.com>
2021-09-10 09:40:37 +0200

    finally outputs

---
+++ bigfatstring.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let myInput;
+let myString;
+
+document.querySelector("button").addEventListener("click", generate);
+const options = document.querySelector("#options");
+const inputBox = document.querySelector("#myInput");
+const outputBox = document.querySelector("#myOutput");
+
+function generate() {
+  console.log("generate option");
+  myInput = inputBox.value;
+  outputBox.value = "";
+
+  if (options.value === "opt1") {
+    myInput = myInput.toLowerCase();
+    myString =
+      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
+    showResult();
+  }
+}
+
+function showResult() {
+  console.log(myString);
+  outputBox.value = myString;
+}
