84f41936614f36596afadff2650d47a722b29e9a
carolynegreen <carolynemarie94@gmail.com>
2021-09-13 17:20:19 +0200

    option 3 and 4 but 4 is not complete

--- bigfatstring.js
+++ bigfatstring.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let myInput;
-let myString;
+let myResult;
 
 document.querySelector("button").addEventListener("click", generate);
 const options = document.querySelector("#options");
@@ -16,21 +16,35 @@ function generate() {
   if (options.value === "opt1") {
     //If input is a first name: Make the first character in a name uppercase, and the rest lowercase
     myInput = myInput.toLowerCase();
-    myString =
+    myResult =
       myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
   }
   if (options.value === "opt2") {
     //If input is a full name: Find the first name
-    myString = myInput.split(" ")[0];
+    myResult = myInput.split(" ")[0];
   }
   if (options.value === "opt3") {
     //If input is a full name: Find the length of the first name
-    myString = myInput.split(" ")[0];
+    myResult = myInput.split(" ")[0].length;
+  }
+  if (options.value === "opt4") {
+    //If input is a full name: Find the middle name start and end position, and the middle name itself in a full name string
+    myResult = myInput.substring(
+      myInput.indexOf(" ") + 1,
+      myInput.lastIndexOf(" ")
+    );
+  }
+  if (options.value === "opt5") {
+    //If input is a filename: Check if a filename is .png or .jpg
+    myResult = myInput.substring(
+      myInput.indexOf(" ") + 1,
+      myInput.lastIndexOf(" ")
+    );
   }
   showResult();
 }
 
 function showResult() {
-  console.log(myString);
-  outputBox.value = myString;
+  console.log(myResult);
+  outputBox.value = myResult;
 }
