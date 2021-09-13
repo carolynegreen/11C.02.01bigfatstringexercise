fa8b830a9c81157d79c91857b7580e189564aaee
carolynegreen <carolynemarie94@gmail.com>
2021-09-13 16:46:36 +0200

    option 1 and 2 working

--- bigfatstring.js
+++ bigfatstring.js
@@ -14,44 +14,18 @@ function generate() {
   outputBox.value = "";
 
   if (options.value === "opt1") {
+    //If input is a first name: Make the first character in a name uppercase, and the rest lowercase
     myInput = myInput.toLowerCase();
     myString =
       myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
   }
   if (options.value === "opt2") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
+    //If input is a full name: Find the first name
+    myString = myInput.split(" ")[0];
   }
   if (options.value === "opt3") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
-  }
-  if (options.value === "opt4") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
-  }
-  if (options.value === "opt5") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
-  }
-  if (options.value === "opt6") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
-  }
-  if (options.value === "opt7") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
-  }
-  if (options.value === "opt8") {
-    myInput = myInput.toLowerCase();
-    myString =
-      myInput.charAt(0).toUpperCase() + myInput.substring(1).toLowerCase();
+    //If input is a full name: Find the length of the first name
+    myString = myInput.split(" ")[0];
   }
   showResult();
 }
